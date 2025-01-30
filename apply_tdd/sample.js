00881cb3920f45c2dfd28d9031ae2373c2e55dd9
sapierre26 <122914227+sapierre26@users.noreply.github.com>
2025-01-30 09:03:19 -0800

    Delete apply_tdd directory

--- apply_tdd/sample.js
+++
@@ -1,6 +0,0 @@
-function multipliesNumbers(num1, num2) {
-	return num1 * num2;
-
-}
-
-exports.multipliesNumbers = multipliesNumbers;
--- apply_tdd/sample.test.js
+++
@@ -1,5 +0,0 @@
-const multiply = require('./sample.js');
-
-test('Testing multiplication - normal test', () => {
-	expect(24).toBe(multiply.multipliesNumbers(3, 7));
-});
